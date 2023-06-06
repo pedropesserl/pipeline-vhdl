692cc26739a2c3a4b72b8775401f38b1bb97a527
Luize Duarte <luize.duarte@ufpr.br>
2023-06-06 12:18:37 -0300

    Update ALUControl.vhd

--- EX/ALUControl.vhd
+++ EX/ALUControl.vhd
@@ -7,17 +7,13 @@ entity ALUControl is port(
 	func 				: in std_logic_vector(3 downto 0);
 	ALUControlOut		 	: out std_logic_vector(3 downto 0)
 );
+end ALUControl;
 
 architecture Behavioral of ALUControl is
 begin 
-	if (ALUop = "00") then
-		ALUControlOut <= "0000"; 	--soma
-	elsif (ALUop = "01") then
-		ALUControlOut <= "0001"; 	--subtracao
-	elsif (ALUop = "10") then
-		ALUControlOut <= ???; 		--set less than
-	else 
-		ALUControlOut <= func; 		--instrucao tipo R
-	end if;
-
+	with ALUop select ALUControlOut <= 
+		"0000" when "00",				--soma
+		"0001" when "01",				--subtracao
+		??? when "10",					--set less than
+		func when others;				--instrucao tipo R
 end Behavioral;

7735b89aa4c7efe28b7c5f9041eeaaf3a8cc1f89
Luize Duarte <luize.duarte@ufpr.br>
2023-06-06 12:18:52 -0300

    Update calc_BeqAdress.vhd

--- EX/calc_BeqAdress.vhd
+++ EX/calc_BeqAdress.vhd
@@ -3,13 +3,14 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_signed.all;
 
 entity calc_BeqAdress is port(
-	newPC, im_estendido			: in std_logic_vector(15 downto 0);
+	newPC, imm_extended			: in std_logic_vector(15 downto 0);
 	BeqAddress				: out std_logic_vector(15 downto 0)
 );
+end calc_BeqAdress;
 
 architecture Behavioral of calc_BeqAdress is
 begin
-	--pc novo + imediato shiftado 2 bits
-	BeqAddress <= newPC + (im_estendido(13 downto 0) & "00");
+	--pc novo + imm shifted 2 bits
+	BeqAddress <= newPC + (imm_extended(13 downto 0) & "00");
 
 end Behavioral;

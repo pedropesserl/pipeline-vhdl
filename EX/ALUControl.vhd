ee416cce87624c2cc9ee2cdf2d6cc029da88399e
Luize Duarte <luize.lcd@hotmail.com>
2023-06-14 09:37:06 -0300

    ula

--- EX/ALUControl.vhd
+++ EX/ALUControl.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_signed.all;
 
 entity ALUControl is port(
 	ALUop 				: in std_logic_vector(1 downto 0);
-	func 				: in std_logic_vector(3 downto 0);
+	func 				: in std_logic_vector(2 downto 0);
 	ALUControlOut		 	: out std_logic_vector(3 downto 0)
 );
 end ALUControl;
@@ -12,8 +12,8 @@ end ALUControl;
 architecture Behavioral of ALUControl is
 begin 
 	with ALUop select ALUControlOut <= 
-		"0000" when "00",				--soma
-		"0001" when "01",				--subtracao
-		??? when "10",					--set less than
+		"000" when "00",				--soma
+		"001" when "01",				--subtracao
+		"101" when "10",				--set less than
 		func when others;				--instrucao tipo R
 end Behavioral;
--- EX/ula.vhd
+++ EX/ula.vhd
@@ -13,10 +13,17 @@ end ula;
 
 architecture Behavioral of ula is
 begin
-
-	--ver clock
-
-	--colocar os ifs para cada tipo de operacao
+	case ALUControlOut is
+		when "000" => ALUOut <= data_a + data_b;
+		when "001" => ALUOut <= data_a - data_b;
+		when "010" => ALUOut <= data_a and data_b;
+		when "011" => ALUOut <= data_a or data_b;
+		when "100" => ALUOut <= data_a nor data_b;
+		when "101" => ALUOut <= data_a < data_b;
+		when "110" => ALUOut <= data_a sll to_integer(unsigned(shamt));
+		when "111" => ALUOut <= data_a srl to_integer(unsigned(shamt));
+		when others => ALUOut <= (others => '0');
+	end case;
 
 	zero <= '1' when ALUOut = "0" else 
 		'0';

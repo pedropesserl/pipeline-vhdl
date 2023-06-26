5db7f1a6c3fbaf09d8318450aad6b80f3b2d951c
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-06-25 23:06:54 -0300

    [ID/RegisterBank] sintaxe

--- ID/RegisterBank.vhd
+++ ID/RegisterBank.vhd
@@ -3,9 +3,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity RegisterBank is port(
-    Rs, Rt, Rd, Data: in std_logic_vector(15 downto 0);
-    RegWrite, clk   : in std_logic;
-    DataA, DataB    : out std_logic_vector(15 downto 0)
+    Rs, Rt, Rd   : in std_logic_vector(1 downto 0);
+    Data         : in std_logic_vector(15 downto 0);
+    RegWrite, clk: in std_logic;
+    DataA, DataB : out std_logic_vector(15 downto 0)
 );
 end RegisterBank;
 
@@ -17,8 +18,9 @@ architecture Behavioral of RegisterBank is
     );
     end component;
 
+    type Reg_out_type is array (0 to 3) of std_logic_vector(15 downto 0);
+    signal Reg_out: Reg_out_type := (others => (others => '0'));
     signal WE     : std_logic_vector(3 downto 0);
-    signal Reg_out: array (0 to 3) of std_logic_vector(15 downto 0);
 begin
     with Rd select WE <=
         "000" & RegWrite      when "00",
@@ -28,20 +30,20 @@ begin
 
     gen_registers: for i in 0 to 3 generate
         r: Register16 port map (
-            clk => clk;
-            Data_in => Data;
-            Write_Enable => WE(i);
+            clk => clk,
+            Data_in => Data,
+            Write_Enable => WE(i),
             Data_out => Reg_out(i)
         );
     end generate;
 
-    with Rs select DA_out <=
+    with Rs select DataA <=
         Reg_out(0) when "00",
         Reg_out(1) when "01",
         Reg_out(2) when "10",
         Reg_out(3) when "11";
 
-    with Rt select DB_out <=
+    with Rt select DataB <=
         Reg_out(0) when "00",
         Reg_out(1) when "01",
         Reg_out(2) when "10",
--- Register16.vhd
+++ Register16.vhd
@@ -7,12 +7,14 @@ entity Register16 is port(
     Write_Enable, clk: in std_logic;
     Data_out         : out std_logic_vector(15 downto 0)
 );
+end Register16;
 
 architecture Behavioral of Register16 is
 begin
-    process(clk) is
-        if falling_edge(clk) and Write_enable = '1' then
-            Data_out <= Data_in;
+    process(clk, Write_Enable) is
+	begin
+        if falling_edge(clk) and Write_Enable = '1' then
+		    Data_out <= Data_in;
         end if;
     end process;
 end Behavioral;

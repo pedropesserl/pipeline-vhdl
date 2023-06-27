a2c79e29708be9db9645df2f18bc9be2d94a65b0
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-06-27 10:12:16 -0300

    [ID] renomear coisas no register bank

--- ID/RegisterBank.vhd
+++ ID/RegisterBank.vhd
@@ -3,10 +3,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity RegisterBank is port(
-    Rs, Rt, Rd   : in std_logic_vector(1 downto 0);
-    Data         : in std_logic_vector(15 downto 0);
-    RegWrite, clk: in std_logic;
-    DataA, DataB : out std_logic_vector(15 downto 0)
+    RFonteA, RFonteB, RDest: in std_logic_vector(1 downto 0);
+    Data                   : in std_logic_vector(15 downto 0);
+    RegWrite, clk          : in std_logic;
+    DataA, DataB           : out std_logic_vector(15 downto 0)
 );
 end RegisterBank;
 
@@ -14,12 +14,12 @@ architecture Behavioral of RegisterBank is
     type Regs_t is array (0 to 3) of std_logic_vector(15 downto 0);
     signal Regs: Regs_t := (others => (others => '0'));
 begin
-    DataA <= Regs(to_integer(unsigned(Rs)));
-    DataB <= Regs(to_integer(unsigned(Rt)));
+    DataA <= Regs(to_integer(unsigned(RFonteA)));
+    DataB <= Regs(to_integer(unsigned(RFonteB)));
     process(clk) is
     begin
         if falling_edge(clk) and RegWrite = '1' and Rd /= "00" then
-            Regs(to_integer(unsigned(Rd))) <= Data;
+            Regs(to_integer(unsigned(RDest))) <= Data;
         end if;
     end process;
 end Behavioral;
